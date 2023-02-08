fb1169504d23ca2db6e4cf0f9a339d9301c36ea1
Admin <hidalgo.najos22@triana.salesianos.edu>
2023-02-08 12:26:17 +0100

    Subida de cambios

---
+++ src/main/java/com/salesianos/triana/ComiendoPorTriana/ComiendoPorTrianaApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.ComiendoPorTriana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComiendoPorTrianaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComiendoPorTrianaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianos/triana/ComiendoPorTriana/ComiendoPorTrianaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.ComiendoPorTriana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComiendoPorTrianaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
