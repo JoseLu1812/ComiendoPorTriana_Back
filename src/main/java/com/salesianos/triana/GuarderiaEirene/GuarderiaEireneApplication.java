7d2208efc8ce3ab517ef05bf0905b4aae254e13e
Admin <hidalgo.najos22@triana.salesianos.edu>
2023-02-08 11:45:28 +0100

    Subida del proyecto

---
+++ src/main/java/com/salesianos/triana/GuarderiaEirene/GuarderiaEireneApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.GuarderiaEirene;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuarderiaEireneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuarderiaEireneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/salesianos/triana/GuarderiaEirene/GuarderiaEireneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.triana.GuarderiaEirene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuarderiaEireneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
