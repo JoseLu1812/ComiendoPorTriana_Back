8ed6e14fa2d62fce8f15c074ad8ede415f03364f
Admin <hidalgo.najos22@triana.salesianos.edu>
2023-02-20 14:29:24 +0100

    Subida de import a falta de finalizarlo

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,1 +1,79 @@
-insert into bar ()
+
+
+
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( 53395e14-60d3-46d8-804f-fafd86c72a19, "Joseluhn", "Triana2023", "jlhn@gmail.com", "José Luis Hidalgo Navas", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( c8d2cf0d-1868-4ced-892a-27bea770def6, "aSTracQUat", "b97fC7!Mno", "jiac@email.com", "Jose Ignacio Anaya-Comas", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( a4993c76-a232-442f-ab71-dd3e0342de96, "AgEntRen", "@E@pgK75av", "agp@email.com", "Adolfo García Perez", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( 5cbdc9a6-abb7-4b8d-9766-6edc374ed096, "rENteRLe", "eMe1_@7dmQ", "pmm@email.com", "Paola Madrid Morata", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( 117b5abd-b3e2-4b5e-a12a-f02b76204aee, "hINglING", "Ct7cF9_fXp", "smc@email.com", "Sebastian Mateo Casals", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( 683cf076-0f88-4838-8e4f-91de1a78cfae, "MSBIaLWa", "_Wz4AhD7O3", "yrv@email.com", "Yolanda Riera-Vidal", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( 12b34a1e-fdbf-4aa6-8ee6-a441b2312193, "AwaciCha", "!odLg4XLg8", "nec@email.com", "Néstor Escudero Calatayud", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( bbc620d2-b534-4e6f-91bf-8163a164df43, "OUsTiLan", "ugjsh5!XGG", "agf@email.com", "Antonia Galiano Frías", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( 11e2191e-2056-43e7-be3b-b9b77066739e, "anbElEyE", "Zgnk4@GU9r", "pfp@email.com", "Pedro Ferrer Peiró", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( 323cddc9-09a1-4107-8e6d-9199aa2f7baf, "ANgTAtrO", "yjF_o9X_pE", "rcm@email.com", "Roque Cruz Manzanares", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( 9f771c2e-01b0-4fe1-adb7-45aa09eb4c1e, "GLAndALV", "v3Ct@mk5_J", "omh@email.com", "Óscar Malo Hernandez", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( e31194db-3c18-47a5-bfdb-172ceebf7660, "tOMpOloy", "AX5@r2FGxD", "pss@email.com", "Pilar Serna Salazar", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( e70c9108-ddcf-416b-a957-01feed566a64, "couNorIs", "xzFt7pjJR@", "afm@email.com", "Amalia Fuster Mata", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( 062a6a92-98eb-43c8-bbc0-4089b36962a4, "psiMPInu", "@OdwAUz_3_", "ndf@email.com", "Nacho Dominguez Fuentes", true, true, true, true, , );
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( d9554666-0096-469d-96c2-eb20e31dcb23, "MOnfIrCE", "e8X9iw!a2C", "ats@email.com", "Amancio Tristán Solís", true, true, true, true, , );
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( d5794d4c-bd38-4f6d-8462-056072d881d4, "EANKleoP", "iP5xyw!Yjy", "amb@email.com", "Ainoa Meléndez Bayo", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( fb36c6a8-ea13-408e-8646-146838dae8ac, "NtIveROn", "nm!kQ3_HYX", "bnp", "Blas Novoa Plana", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( cd728c15-3f48-4104-994b-956560e52f82, "ApAuTaBe", "dMH_09hu26", "alar@email.com", "Ángela García Pérez", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( b4b2af1f-cbfa-4f2e-8039-33b522f4e211, "rSHENCER", "fu9Tz4@O2k", "ngt@email.com", "Nuria Gallego Trujillo", true, true, true, true, ,);
+insert into user (id, username, password, email, full_name, account_non_locked, account_non_locked, credentials_non_expired, enabled, created_at, last_password_change_at) values ( 9d6076d7-233d-4111-bd19-1c589d2a2764, "Pilarnt", "Triana2023", "mpnt@gmail.com", "maría del Pilar Navas Tenor", true, true, true, true, ,);
+
+
+insert into bar (id, name, description, direction, owner, images, created_at) values ( c2dde920-80df-4743-86d4-3545269edcb7, "Casa Ruperto", "Este clásico bar trianero lleva desde 1970 sirviendo codornices fritas y adobadas y son los 'pajaritos' más celebrados de toda Sevilla. Todo aquel que para en este singular templo de las codornices se va encantado tanto por calidad de la comida como por la simpatía y el buen ambiente que se vive en Casa Ruperto.", "Avenida Santa Cecilia, 2", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( db1ac247-571a-43cf-82ce-411728783c07, "Bar Casa Casimiro ", "Tienen una carta amplia en las que se incluye: croquetas, huevos de chocos, flamenquín, solomillo al whisky, filete de pollo bechamel, entre otras.", "Avenida de Coria, 19", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( 464f01c6-7f8b-4afb-b9ba-44fdf9db9631, "Trianilla", "Su amplia carta tiene manjares tales como: ensaladilla de pulpo, coca de gorgonzola, dados de atún rojo sobre pan, puntas de solomillo ibérico y hamburguesa de ternera black angus, entre otros platos.", "C/Manuel Pareja Obregón, 2", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( ae6d3c89-07ae-4e73-ad79-97148ca40158, "Bar Salomón Rey de los Pinchitos", "Aquí la especialidad de la casa son los pinchitos, como su nombre bien indica. Hay tapas y raciones para degustar tanto en el salón como en la terraza. Además de los pinchitos tienen gran fama las patatas bravas, la ensaladilla, el calabacín relleno, zanahorias aliñadas, solomillo de atún mechado o los mejillones en escabeche de las rías bajas.", "López de Gómara, 11", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( 625cd031-1a54-4d3d-a6bc-b835909fa18e, "Café Bar Altozano de Triana", "Es de los bares clásicos del barrio y su comida más clásica todavía. Destaca sin duda por sus tortillitas de camarones, el adobo y las coquinas. Es imposible que este bar defraude a alguien. Además tiene un no sé qué especial que hace que te sientas trianero. Por no hablar de sus bajos precios.", "C/San Jacinto, 4", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( 3bb95209-da68-4f55-9eed-ba0001a1ad3c, "Blanca Paloma", "De los mejores lugares trianeros con diferencia. No es baratísimo pero merece mucho la pena por algunas de las exquisiteces que puedes tomar. Además dentro de la comida clásica suelen ser innovadores.", "C/San Jacinto, 49", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( bf260a9b-3438-4067-b3d2-030b782b1388, "Victoria 8", "Lugar recomendadísimo en todos los sentidos. El local está bien decorado y es acogedor. No hay una gran variedad pero eso es lo de menos, porque lo compensa con creces sus tataki de atún y sus croquetas de rabo de toro.", "C/Victoria, 8", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( 9f26b992-027b-4290-8851-34bc5c40e36f, "Sol y sombra", "Otro tipiquísimo lugar del barrio de los que huelen a queso curado y jamón. Hay una gran variedad de tapas y de precio es más que correcto.", "C/Castilla, 151", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( 7055f932-a1ea-4fe7-a4b2-0da9a871f644, "Vega 10", "El macarrón toro, las arepas y la pastela son algunas de las mejores comidas del lugar, por no hablar de los postres. Dejad un hueco (aunque sea complicado) para el cremoso de azafrán.", "C/Vega, 10", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( a7b81431-43a1-4e72-8685-3647f12b3047, "Maria Trifulca", "Aunque solo sea por sus espectaculares vistas, bien merece la pena incluirlo en esta lista. Ubicado en el antiguo faro de Triana, Maria Trifulca es una comida de lujo sobre el Guadalquivir y la opción perfecta para ocasiones especiales.", "Puente de Triana, Plaza del Altozado, esquina", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( bf7c2b27-6396-4abf-8eed-b93933290621, "Taberna Miami", "Una taberna donde comer en Triana de tapas o de raciones sentado en su terraza. No te pierdas el adobo, las acedías o el secreto. También recomiendan las bandejas de pescaito frito, el rabo de toro y el arroz negro.", "C/San Jacinto, 21", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( e0469d55-f941-44d0-93ae-1ffed49d300c, "Casa Diego", "Una tasca tradicional de las más típicas y uno de los mejores bares de tapas en Triana. Comida casera, con buenas raciones y de calidad.", "C/Calle Alfarería, 5", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( 912b2cb8-389f-408a-889d-95ea65250e67, "Patio de San Eloy", "Un lugar emblemático donde tomar algo, también imprescindible en el top ten de bares y restaurantes donde comer de tapas en Triana bien y barato.", "C/San Jacinto, 16", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( a8046137-373a-40a7-85f3-8d5f241bbceb, "Freiduría Reina Victoria", "Incluimos esta freiduría entre las recomendaciones para comer de tapas en Triana bien y barato, tal y como nos la dieron. Aunque tienen una pequeña sala para comer, en realidad el local es muy pequeño y lo ideal es coger la comida para llevar.", "C/Rodrigo de Triana, 51", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( 63d502f3-3c5e-4eef-ae9c-21840e243faf, "", "", "", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( a3c714bb-8588-4591-a7d1-3fa058304c9c, "", "", "", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( 80adfa7a-6a63-42f3-8a1e-153039544e7b, "", "", "", , "", );
+insert into bar (id, name, description, direction, owner, images, created_at) values ( b8ccde1e-59c7-4245-8e13-264e76beb728, "", "", "", , "", );
+
+
+
+
+
+insert into favoritos(bar_id, user_id) values ( , d9554666-0096-469d-96c2-eb20e31dcb23);
+insert into favoritos(bar_id, user_id) values ( , d9554666-0096-469d-96c2-eb20e31dcb23);
+insert into favoritos(bar_id, user_id) values ( , d9554666-0096-469d-96c2-eb20e31dcb23);
+insert into favoritos(bar_id, user_id) values ( , d9554666-0096-469d-96c2-eb20e31dcb23);
+insert into favoritos(bar_id, user_id) values ( , d5794d4c-bd38-4f6d-8462-056072d881d4);
+insert into favoritos(bar_id, user_id) values ( , d5794d4c-bd38-4f6d-8462-056072d881d4);
+insert into favoritos(bar_id, user_id) values ( , d5794d4c-bd38-4f6d-8462-056072d881d4);
+insert into favoritos(bar_id, user_id) values ( , d5794d4c-bd38-4f6d-8462-056072d881d4);
+insert into favoritos(bar_id, user_id) values ( , d5794d4c-bd38-4f6d-8462-056072d881d4);
+insert into favoritos(bar_id, user_id) values ( , cd728c15-3f48-4104-994b-956560e52f82);
+insert into favoritos(bar_id, user_id) values ( , cd728c15-3f48-4104-994b-956560e52f82);
+insert into favoritos(bar_id, user_id) values ( , b4b2af1f-cbfa-4f2e-8039-33b522f4e211);
+insert into favoritos(bar_id, user_id) values ( , b4b2af1f-cbfa-4f2e-8039-33b522f4e211);
+insert into favoritos(bar_id, user_id) values ( , b4b2af1f-cbfa-4f2e-8039-33b522f4e211);
+insert into favoritos(bar_id, user_id) values ( , 9d6076d7-233d-4111-bd19-1c589d2a2764);
+insert into favoritos(bar_id, user_id) values ( , 9d6076d7-233d-4111-bd19-1c589d2a2764);
+insert into favoritos(bar_id, user_id) values ( , 9d6076d7-233d-4111-bd19-1c589d2a2764);
+insert into favoritos(bar_id, user_id) values ( , 9d6076d7-233d-4111-bd19-1c589d2a2764);
+insert into favoritos(bar_id, user_id) values ( , 9d6076d7-233d-4111-bd19-1c589d2a2764);
+
+
+
+
+comment (id, text, title, author, bar, created_at) values ( , "", "", , , );
+
+
+
+
+
+
+
